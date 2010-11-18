1fffa2db3c599ab3e198e95e8c4b5e292f6fa30e
Venkata Koppaka <subbarao.cse@gmail.com>
2010-11-17 22:10:25 -0500

    Version 1.0 for Save the World - Ninja! App.
    Signed-off-by: Venkata Koppaka <subbarao.cse@gmail.com>

--- HelloWorld-WindowsPhone/MainPage.xaml.cs
+++ HelloWorld-WindowsPhone/MainPage.xaml.cs
@@ -20,5 +20,25 @@ namespace HelloWorld_WindowsPhone
         {
             InitializeComponent();
         }
+
+        private void btnSaveTheWorld_Click(object sender, RoutedEventArgs e)
+        {
+            string ninjaPhrase = tbNinjaPhrase.Text;
+            if (!string.IsNullOrEmpty(ninjaPhrase))
+            {
+                if (string.Compare("startrek", ninjaPhrase, StringComparison.OrdinalIgnoreCase) == 0)
+                {
+                    messageBlock.Text = "Vola! Way to go Ninja, You saved the world!";
+                }
+                else
+                {
+                    messageBlock.Text = "That is not a Ninja Phrase! You cannot save the world!";
+                }
+            }
+            else
+            {
+                messageBlock.Text = "Are you kidding? That is not a Ninja Phrase! You cannot save the world!";
+            }
+        }
     }
 }
